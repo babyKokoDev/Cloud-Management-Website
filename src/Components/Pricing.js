f067cebe8098ef0fc253be0624d8c6764e9ecb8b
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-12 16:17:35 +0100

    pricing section completed

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -13,21 +13,37 @@ const Pricing = () => {
             </div>
 
           <div className='grid md:grid-cols-2'>
-            <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl'>
+            <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
                 <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Standard</span>
                 <div>
                     <p className='text-6xl font-bold py-4 flex'>$49<span className='text-xl text-slate-[500] flex items-end'>/mo</span></p>
                 </div>
                 <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <div>
-                    <p><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
-                    <p><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
-                    <p><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
-                    <p><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
-                    <p><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
-                    <button>Get Started</button>
+                <div className='text-2xl'>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <button className='w-full py-4 my-4'>Get Started</button>
                 </div>
             </div>
+            <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+                <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Premium</span>
+                <div>
+                    <p className='text-6xl font-bold py-4 flex'>$99<span className='text-xl text-slate-[500] flex items-end'>/mo</span></p>
+                </div>
+                <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <div className='text-2xl'>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                    <button className='w-full py-4 my-4'>Get Started</button>
+                </div>
+            </div>
+            
           </div>
 
         </div>
